cc7904bca9cc4124bcfc87e5e2ddc16d5e0cf274
Akino_Hiroto <rotohi0822@outlook.jp>
2026-05-24 17:58:32 +0900

    The reader is done.

--- csv_reader.py
+++ csv_reader.py
@@ -1,3 +1,11 @@
 import csv
 class CSVReader:
-    pass
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = self.read_csv()
+
+    def read_csv(self):
+        with open(self.file_path, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            data = [row for row in csv_reader]
+        return data
